4154c2e78e2aecd7e2093b4016ec0f306e859362
saliq03 <javidsaliq@gmail.com>
2024-08-10 10:30:12 +0530

    animating camera in google map

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,27 +11,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Completer<GoogleMapController> mapcontroller=Completer();
+  
   static const CameraPosition _kGooglePlex=CameraPosition(
       target: LatLng(17.37244, 78.43782),
-  zoom: 15
-  );
+      zoom: 17);
+  
   List<Marker> _markers=[
     Marker(markerId: MarkerId('1'),
     position: LatLng(17.37244, 78.43782),
    infoWindow:InfoWindow(
      title:'my current location'
-   ))
+   )),
+    
+    Marker(
+        markerId: MarkerId('srinagar'),
+         position: LatLng(34.0837, 74.7973),
+      infoWindow: InfoWindow(title: "Srinagar")
+    )
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-          initialCameraPosition: _kGooglePlex,
-         mapType: MapType.satellite,
-        compassEnabled: true,
-        myLocationEnabled: true,
-        markers: Set<Marker>.of(_markers),
-      ),
+      body: SafeArea(
+        child: GoogleMap(
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller){
+              mapcontroller.complete(controller);
+            },
+           mapType: MapType.normal,
+          compassEnabled: true,
+          myLocationEnabled: true,
+          markers: Set<Marker>.of(_markers),
+        )),
+
+      floatingActionButton: IconButton(
+          onPressed: ()async{
+          GoogleMapController controller=await mapcontroller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(
+                target: LatLng(34.0837, 74.7973),
+            zoom: 13)
+          ));
+          }, icon: Icon(Icons.location_searching,size: 70,)),
     );
   }
 }
